a794663cfb47a2a4ae0308d38802090c1745fd11
Anderson Cavalcante <andersonsilvaacaval@gmail.com>
2024-09-06 16:34:21 -0300

    criando utils da aplicação

--- src/main.py
+++ src/main.py
@@ -1,16 +1,18 @@
 
 from datetime import datetime
 from database.mongo import ConnectMongo
+from src.utils import formatDateAndHour
+
 
 def bootApplication():
     try:
-        print(f"Started in {datetime.now()}")
+        print(f"Started in {formatDateAndHour(datetime.now())}")
         mongo_conn = ConnectMongo("prodDB")
 
     except Exception as error:
         print(f"[main] ERROR: {error}")
     finally:
-        print(f"Finished in {datetime.now()}")
+        print(f"Finished in {formatDateAndHour(datetime.now())}")
 
 
 if __name__ == "__main__":
---
+++ src/utils.py
@@ -0,0 +1,5 @@
+def formatDateAndHour(dateAndHourValue):
+    return dateAndHourValue.strftime("%Y-%m-%d %H:%M")
+
+def formatDate(dateValue):
+    return dateValue.strftime("%Y-%m-%d")
