83475b60dd6508d8771062dfe7d006befe41a07e
Amanda Nobre <amandanobree71@gmail.com>
2024-09-07 20:59:29 -0300

    🚧 implementando uso de variaveis de ambiente

--- src/database/mongo.py
+++ src/database/mongo.py
@@ -1,9 +1,9 @@
 from pymongo import MongoClient
 
 class ConnectMongo:
-    def __init__(self, dbname):
+    def __init__(self, uri, dbname):
         self._dbName = dbname
-        self._conn = MongoClient("mongodb://localhost")
+        self._conn = MongoClient(uri)
         self._db = self._conn[dbname]
 
     def createCollection(self, name):
--- src/main.py
+++ src/main.py
@@ -2,16 +2,20 @@ from datetime import datetime
 from database.mongo import ConnectMongo
 from database.mysql import ConnectMySQL
 from model.order_report import Base
-from src.utils import formatDateAndHour
+from utils import formatDateAndHour
+from dotenv import load_dotenv
+import os
 
 
 def bootApplication():
+    load_dotenv()
+
     try:
         print(f"Started in {formatDateAndHour(datetime.now())}")
-        mongo_conn = ConnectMongo("prodDB")
+        mongo_conn = ConnectMongo(os.getenv('URI_MONGO'), os.getenv('MONGO_DB_NAME'))
 
         mysql_conn = ConnectMySQL(
-            user='root', password='admin321', host='localhost', dbname='order_analysis')
+            user=os.getenv('MYSQL_USER'), password=os.getenv('MYSQL_PASS'), host=os.getenv('MYSQL_HOST'), dbname=os.getenv('MYSQL_DB'))
 
         # Creating order report table
         Base.metadata.create_all(mysql_conn._engine)
@@ -19,6 +23,7 @@ def bootApplication():
     except Exception as error:
         print(f"[main] ERROR: {error}")
     finally:
+        mongo_conn.closeConnection()
         print(f"Finished in {formatDateAndHour(datetime.now())}")
 
 
