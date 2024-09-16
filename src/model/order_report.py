a546a42909e80cc3bef0a71c585e3a4b71367e23
Amanda Nobre <amandanobree71@gmail.com>
2024-09-16 11:28:43 -0300

    👌 Alterações na tabela order_report

--- src/model/order_report.py
+++ src/model/order_report.py
@@ -1,24 +1,27 @@
 from sqlalchemy import Column, Integer, String, Float, DateTime, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
+ModelBase = declarative_base()
 
-class OrderReport(Base):
+
+class OrderReport(ModelBase):
     __tablename__ = 'order_report'
 
     id = Column(Integer, primary_key=True)
-    #Order Dates
+    order_id = Column(String(255), unique=True, nullable=False)
+    # Order Dates
+    created_at = Column(DateTime())
     preparation_start_datetime = Column(DateTime())
     prepared_datetime = Column(DateTime())
     assigned_datetime = Column(DateTime())
     collected_datetime = Column(DateTime())
     datetime_finished = Column(DateTime())
-    #Order Address
+    # Order Address
     street = Column(String(255))
     neighborhood = Column(String(255))
     city = Column(String(255))
     state = Column(String(255))
-    #Payment details and order fee
+    # Payment details and order fee
     additional_fee = Column(Float)
     deliveryman_fee = Column(Float)
     delivery_fee = Column(Float)
@@ -27,7 +30,7 @@ class OrderReport(Base):
     discounts = Column(Float)
     subtotal = Column(Float)
     total = Column(Float)
-    #Order detail
+    # Order detail
     status = Column(String(50))
     type = Column(String(50))
     number = Column(String(50))
