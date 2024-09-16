aa5e07d003612c907ba35e958a3de4cd46ea6537
Amanda Nobre <amandanobree71@gmail.com>
2024-09-16 11:34:13 -0300

    🚧 Criando lógica para buscar último registro em order_report

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
-from datetime import datetime
+from datetime import datetime, time
+
+from bson import ObjectId
+from sqlalchemy import desc
 from database.mongo import ConnectMongo
 from database.mysql import ConnectMySQL
-from model.order_report import Base
+from model.order_report import ModelBase, OrderReport
 from services.order_service import OrderService
 from utils import formatDateAndHour
 from dotenv import load_dotenv
@@ -13,19 +16,38 @@ def bootApplication():
 
     try:
         print(f"Started in {formatDateAndHour(datetime.now())}")
-        mongo_conn = ConnectMongo(os.getenv('URI_MONGO'), os.getenv('MONGO_DB_NAME'))
+        mongo_conn = ConnectMongo(
+            os.getenv('URI_MONGO'), os.getenv('MONGO_DB_NAME'))
         orderService = OrderService(mongo_conn)
-        
+
         mysql_conn = ConnectMySQL(
             user=os.getenv('MYSQL_USER'), password=os.getenv('MYSQL_PASS'), host=os.getenv('MYSQL_HOST'), dbname=os.getenv('MYSQL_DB'))
 
         # Creating order report table
-        Base.metadata.create_all(mysql_conn._engine)
-        fetchedOrders = orderService.fetchOrders({})
-        
-        df_orders = orderService.processOrders(fetchedOrders)
-        df_orders.to_sql('orders', con=mysql_conn._engine,
-                         if_exists='append', index=False)
+        ModelBase.metadata.create_all(mysql_conn._engine)
+
+        # Mongo query
+        query = {}
+
+        # Retrieving the date of the last order registered in mysql
+        last_register = mysql_conn._session.query(OrderReport).order_by(
+            desc(OrderReport.created_at)).first()
+
+        if (last_register):
+            if (last_register.created_at):
+                query = {"createdAt": {
+                    "$gte": datetime.combine(last_register.created_at.date(), time.min)}}
+
+        # Fetching order in Mongo DB By Query
+        print("Fetching orders...")
+        fetchedOrdersByQuery = orderService.fetchOrders(query)
+
+        # Creating order report dataframe
+        print("Creating DataFrame and Inserting...")
+        df_orders = orderService.processOrders(fetchedOrdersByQuery)
+        df_orders.to_sql(OrderReport.__tablename__, con=mysql_conn._engine,
+                         if_exists='append', index=False, method='multi')
+
     except Exception as error:
         print(f"[main] ERROR: {error}")
     finally:
